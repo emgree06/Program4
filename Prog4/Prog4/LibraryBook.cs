cf423a81ddc74e8b25a42d3845932b3f198d1823
emgree06 <44639898+emgree06@users.noreply.github.com>
2018-10-31 08:42:49 -0400

    Add files via upload

---
+++ Prog4/Prog4/LibraryBook.cs
@@ -0,0 +1,136 @@
+﻿/*
+ * Grade ID: A4431
+ * Progam 4
+ * Due: 12/5/2017
+ * Section 01
+ * */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog4
+{
+    public class LibraryBook
+    {
+        private string _title;//backing field for Title
+        private string _author;//Backing field for Author
+        private string _publisher;//Backing field for Publisher
+        private string _callnum;//Backing field for CallNum
+
+        private int _copyrightyear;//Backing field for CopyRightYear
+
+        private bool _checkedout = false;//Books are automatically not checked out therefore it is set to false
+
+        //Pre-condition: None
+        //Post-condition: return Title
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        //Pre-condition: None
+        //Post-condition: return author
+        public string Author
+        {
+            get { return _author; }
+            set { _author = value; }
+        }
+
+        //Pre-Condition: None
+        //Post-condition: return publisher
+        public string Publisher
+        {
+            get { return _publisher; }
+            set { _publisher = value; }
+        }
+
+        //Pre-condtion: none
+        //Post-condtion: return call number
+        public string CallNum
+        {
+            get { return _callnum; }
+            set { _callnum = value; }
+        }
+
+        //Pre-condition: the value for copyright year > 0
+        //Post-condition: return value for copyright year
+        public int CopyRightYear
+        {
+            get
+            {
+                return _copyrightyear;
+            }
+            set
+            {
+                if (value <= 0)//if copyright year is negative or equal to zero the year will be set to 2017
+                {
+                    _copyrightyear = 2017;
+                }
+                else
+                    _copyrightyear = value;
+            }
+
+        }
+
+        //Pre-condition: the book shouldn't be checked out AKA _checkedout ==false
+        //Post-Condition: the book should be checked out AKA change _checkedout to true
+        public void CheckOut()
+        {
+            if (_checkedout == false)
+            {
+                _checkedout = true;
+            }
+        }
+
+        //Pre-condition: book should be checked out AKA _checkedout == true
+        //Post-condition: _checkedout == false and book isn't checked out
+        public void ReturnToShelf()
+        {
+            if (_checkedout == true)
+            {
+                _checkedout = false;
+            }
+        }
+
+        //Pre-condition: None
+        //Post-condition: return value of true or false
+        public bool IsCheckedOut()
+        {
+            if (_checkedout == false)
+            {
+                return false;
+            }
+            else
+                return true;
+        }
+
+        //constructor for Librarybook
+        //Pre-condition: every book needs a titel, author, publisher, call number, and copyright year
+        //Post-condition: A LibraryBook will be initialized with a title, author, publisher, call number, and copyright year
+        public LibraryBook(string title, string author, string publisher, string callnum, int copyrightyear)
+        {
+            Title = title;//set using Title property
+            Author = author;// set using Author property
+            Publisher = publisher; //set using Publisher property
+            CallNum = callnum;// set using CallNum property
+            CopyRightYear = copyrightyear;//set using CopyRightYear property
+
+
+        }
+
+        //Represents data as a string to the Console
+        public override string ToString()
+        {
+            return $"Title:             {Title} {Environment.NewLine}"
+                + $"Author:             {Author} {Environment.NewLine}"
+                + $"Publisher:          {Publisher} {Environment.NewLine}" +
+                $"Call number:         {CallNum}{Environment.NewLine}" +
+                $"Copyright Year:     {CopyRightYear}{Environment.NewLine}" +
+                $"Checked out:         {IsCheckedOut()}";
+        }
+    }
+}
+
---
+++ Prog4/Prog4/Test.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Prog4
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            //creating data for books
+            LibraryBook book1 =
+                new LibraryBook("Wealth of Nations", "Adam Smith", "Penguin House", "LB1234", 1776);
+            LibraryBook book2 =
+                new LibraryBook("The Bell Jar", "Silva Plath", "Harper Collins", "LB5678", 1996);
+            LibraryBook book3 =
+                new LibraryBook("Sense and Sensibility", "Jane Austen", "Barnse and Noble", "LB9101", 2003);
+            LibraryBook book4 =
+                new LibraryBook("Pride and Predjudice", "Jane Austen", "W.W. Norton and Company", "LB1213", 2001);
+            LibraryBook book5 =
+                new LibraryBook("Emma", "Jane Austen", "Penguin House", "LB1415", 1996);
+
+            //Pre-condition: none
+            //Post-condition: 5 library books are put in an array
+            LibraryBook[] books = new LibraryBook[5];
+            books[0] = book1;
+            books[1] = book2;
+            books[2] = book3;
+            books[3] = book4;
+            books[4] = book5;
+
+            WriteLine("***********Starting Data***********");
+            WriteLine();
+            //Pre-condition: none
+            //Post-condition: prints data in array books to console
+            foreach (LibraryBook book in books)//goes through the array and prints the data to the console
+            {
+                WriteLine(book);
+                WriteLine();
+            }
+
+            //changing data of books
+
+            //Two books checked out
+            book1.CheckOut();
+            book2.CheckOut();
+
+            //Copyright year validation check
+            book4.CopyRightYear = -2010;
+            book3.CopyRightYear = 0;
+
+            //Changing call number or publisher
+            book1.Publisher = "Mary Had a Little Lamb";
+            book2.CallNum = "LB1617";
+            book3.CallNum = "LB1819";
+            book4.Publisher = "Quack House";
+            book5.Publisher = "Brick Wall";
+
+            WriteLine();
+            WriteLine("***********Changed Data***********");
+            WriteLine();
+            //precondition: None
+            //Post-condition: prints data in array books to console
+            foreach (LibraryBook book in books)//goes through the array and prints the data to the console
+            {
+                WriteLine(book);
+                WriteLine();
+            }
+
+            WriteLine();
+            WriteLine("*********Returning Checked out Books**********");
+            WriteLine();
+
+            //returning books to the library
+            book1.ReturnToShelf();
+            book2.ReturnToShelf();
+
+            //Pre-condition: none
+            //Post-condition: prints data in array books to console
+            foreach (LibraryBook book in books)//goes through array and prints the data to the console
+            {
+                WriteLine(book);
+                WriteLine();
+            }
+        }
+    }
+}
+
